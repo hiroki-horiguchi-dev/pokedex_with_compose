8b675e5dba339e7e1a0d86278c58ce535ab10aca
hiroki.horiguchi <hiroki.horiguchi@GLM00272noMacBook-Pro.local>
2024-05-09 20:10:08 +0900

    雑いけどコミット

--- app/src/main/java/com/example/pokedex_with_compose/network/service/PokeDexClient.kt
+++ app/src/main/java/com/example/pokedex_with_compose/network/service/PokeDexClient.kt
@@ -3,9 +3,6 @@ package com.example.pokedex_with_compose.network.service
 import PokedexService
 import com.example.pokedex_with_compose.model.PokemonInfo
 import com.example.pokedex_with_compose.network.model.PokemonResponse
-import retrofit2.http.GET
-import retrofit2.http.Path
-import retrofit2.http.Query
 import javax.inject.Inject
 
 class PokedexClient @Inject constructor(
