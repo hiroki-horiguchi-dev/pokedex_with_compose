031c4680196af2a6d64951ae03648c9c5df293c2
hiroki.horiguchi <hiroki.horiguchi@GLM00272noMacBook-Pro.local>
2024-05-09 19:58:26 +0900

    間違えて interface 作ってた、てへ

--- app/src/main/java/com/example/pokedex_with_compose/network/service/PokeDexClient.kt
+++ app/src/main/java/com/example/pokedex_with_compose/network/service/PokeDexClient.kt
@@ -1,18 +1,29 @@
 package com.example.pokedex_with_compose.network.service
 
+import PokedexService
 import com.example.pokedex_with_compose.model.PokemonInfo
 import com.example.pokedex_with_compose.network.model.PokemonResponse
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
+import javax.inject.Inject
 
-interface PokedexService {
-    @GET("pokemon")
-    suspend fun fetchPokemonList(
-        @Query("limit") limit: Int = 20,
-        @Query("offset") offset: Int = 0,
-    ): PokemonResponse
+class PokedexClient @Inject constructor(
+    private val pokedexService: PokedexService,
+) {
 
-    @GET("pokemon/{name}")
-    suspend fun fetchPokemonInfo(@Path("name") name: String): PokemonInfo
+    suspend fun fetchPokemonList(page: Int): PokemonResponse =
+        pokedexService.fetchPokemonList(
+            limit = PAGING_SIZE,
+            offset = page * PAGING_SIZE,
+        )
+
+    suspend fun fetchPokemonInfo(name: String): PokemonInfo =
+        pokedexService.fetchPokemonInfo(
+            name = name,
+        )
+
+    companion object {
+        private const val PAGING_SIZE = 20
+    }
 }
