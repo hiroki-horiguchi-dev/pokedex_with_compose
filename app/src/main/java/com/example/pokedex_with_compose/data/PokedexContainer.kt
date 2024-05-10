444fc4bae101f1f49ab01bd16f0b217c5df8281b
hiroki.horiguchi <hiroki.horiguchi@GLM00272noMacBook-Pro.local>
2024-05-10 13:14:56 +0900

    http通信監視用クラスの追加

--- app/src/main/java/com/example/pokedex_with_compose/data/PokedexContainer.kt
+++ app/src/main/java/com/example/pokedex_with_compose/data/PokedexContainer.kt
@@ -1,7 +1,9 @@
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
+import com.example.pokedex_with_compose.network.interseptor.HttpRequestInterceptor
 
 interface PokedexContainer {
     val pokemonRepository: PokedexRepository
@@ -10,7 +12,12 @@ interface PokedexContainer {
 class PokedexDefaultContainer: PokedexContainer {
     private val BASE_URL = "https://pokeapi.co/api/v2/"
 
+    private val okHttpClient = OkHttpClient.Builder()
+        .addInterceptor(HttpRequestInterceptor())
+        .build()
+
     private val pokemonRetrofit = Retrofit.Builder()
+        .client(okHttpClient)
         .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
         .baseUrl(BASE_URL)
         .build()
