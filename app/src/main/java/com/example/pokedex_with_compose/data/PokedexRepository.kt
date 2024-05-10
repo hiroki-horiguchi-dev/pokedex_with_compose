d4f11712e75bcde2487c9acb584cd3575bf254e6
hiroki.horiguchi <hiroki.horiguchi@GLM00272noMacBook-Pro.local>
2024-05-10 16:41:30 +0900

    エラーハンドリングのため retrofit の response を使う

--- app/src/main/java/com/example/pokedex_with_compose/data/PokedexRepository.kt
+++ app/src/main/java/com/example/pokedex_with_compose/data/PokedexRepository.kt
@@ -1,19 +1,23 @@
 import com.example.pokedex_with_compose.model.PokemonInfo
 import com.example.pokedex_with_compose.network.model.PokemonResponse
+import retrofit2.Response
 
 interface PokedexRepository {
     suspend fun fetchPokemonList(
-        page: Int,
-    ): PokemonResponse
+        limit: Int,
+        offset: Int
+    ): Response<PokemonResponse>
 
     suspend fun fetchPokemonInfo(
         name: String
-    ): PokemonInfo
+    ): Response<PokemonInfo>
 }
 
 class NetworkPokedexRepository(
     private val pokedexService: PokedexService
 ) : PokedexRepository {
-    override suspend fun fetchPokemonList(page: Int): PokemonResponse = pokedexService.fetchPokemonList(page)
-    override suspend fun fetchPokemonInfo(name: String): PokemonInfo = pokedexService.fetchPokemonInfo(name)
+    override suspend fun fetchPokemonList(limit: Int, offset: Int): Response<PokemonResponse> = pokedexService.fetchPokemonList(
+        limit, offset
+    )
+    override suspend fun fetchPokemonInfo(name: String): Response<PokemonInfo> = pokedexService.fetchPokemonInfo(name)
 }
