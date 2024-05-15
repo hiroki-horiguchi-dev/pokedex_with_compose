32bad687b23ac8a0c9ba2f0810dbe7962f21c8f3
hiroki.horiguchi <hiroki.horiguchi@GLM00272noMacBook-Pro.local>
2024-05-15 13:13:35 +0900

    不要 import の削除

--- app/src/main/java/com/example/pokedex_with_compose/MainActivity.kt
+++ app/src/main/java/com/example/pokedex_with_compose/MainActivity.kt
@@ -5,12 +5,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.pokedex_with_compose.ui.theme.PokeDex_with_composeTheme
 
